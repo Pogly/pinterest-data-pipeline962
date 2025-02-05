8d49807627cdbbd3836576bdc32a77d13c45333e
pogly <Benjamin.gorham@gmail.com>
2025-02-05 16:01:22 +0000

    Added upload to s3 bucket

--- Pinterest_Data_Pipeline_Benjamin_Gorham/user_posting_emulation.py
+++ Pinterest_Data_Pipeline_Benjamin_Gorham/user_posting_emulation.py
@@ -6,11 +6,16 @@ import boto3
 import json
 import sqlalchemy
 from sqlalchemy import text
-
-
+import yaml
 
 random.seed(100)
 
+with open("Endpoints.yaml") as Endpoint:
+    invoke_url = yaml.safe_load(Endpoint)
+
+def send_To_EC2(Topic,payload):
+     headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+     response = requests.request("POST",f"{invoke_url}{Topic}", headers=headers, data=payload)
 
 class AWSDBConnector:
 
@@ -44,17 +49,48 @@ def run_infinite_post_data_loop():
             for row in pin_selected_row:
                 pin_result = dict(row._mapping)
 
+                payload_pin = json.dumps({
+                "records": [
+                {
+                # Data should be send as pairs of column_name:value, with different columns separated by commas       
+                 "value": {"index": pin_result["index"], "unique_id": pin_result["unique_id"], "title": pin_result["title"], "description": pin_result["description"]
+                           , "poster_name": pin_result["poster_name"], "follower_count": pin_result["follower_count"], "tag_list": pin_result["tag_list"], "is_image_or_video": pin_result["is_image_or_video"]
+                           , "image_src": pin_result["image_src"], "downloaded": pin_result["downloaded"], "save_location": pin_result["save_location"], "category": pin_result["category"]}
+                }]})
+
+                send_To_EC2("e89446818119.pin",payload_pin)
+
+
             geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
             geo_selected_row = connection.execute(geo_string)
             
             for row in geo_selected_row:
                 geo_result = dict(row._mapping)
 
+                payload_geo = json.dumps({
+                "records": [
+                {
+                # Data should be send as pairs of column_name:value, with different columns separated by commas       
+                 "value": {"ind": geo_result["ind"], "timestamp": geo_result["timestamp"], "latitude": geo_result["latitude"], "longitude": geo_result["longitude"], "country": geo_result["country"]}
+                }]}, default=str)
+
+
+                send_To_EC2("e89446818119.geo",payload_geo)
+
             user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
             user_selected_row = connection.execute(user_string)
             
             for row in user_selected_row:
                 user_result = dict(row._mapping)
+                
+                payload_user = json.dumps({
+                "records": [
+                {
+                # Data should be send as pairs of column_name:value, with different columns separated by commas       
+                 "value": {"ind": user_result["ind"], "first_name": user_result["first_name"], "last_name": user_result["last_name"], "age": user_result["age"], "date_joined": user_result["date_joined"]}
+                }]}, default=str)
+
+                send_To_EC2("e89446818119.user",payload_user)
             
             print(pin_result)
             print(geo_result)
@@ -66,5 +102,3 @@ if __name__ == "__main__":
     print('Working')
     
     
-
-
